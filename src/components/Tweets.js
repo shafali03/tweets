41df8816a220f9bab410c4c333eb72242591464d
shafali03 <shaf03sm@gmail.com>
2020-06-19 10:06:46 +0100

    feat: Created a tweet looking examle

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -2,15 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import './Tweet.css'
 import Author from './Author'
+import './Author.css'
 import Message from './Message'
+
 
 function Tweet() {
   return (
     <div className="tweet">
       <Avatar />
       <div className="content">
-        <Author />
+        <Author /><Time />
         <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
       </div>
     </div>
   )
@@ -25,6 +33,29 @@ function Avatar() {
       alt="avatar" />
   )
 }
+
+
+
+const Time = () => (
+  <span className="time">5h ago</span>
+)
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button" />
+)
+
+
+const RetweetButton = () => (
+  <i className="fa fa-retweet retweet-button" />
+)
+
+const LikeButton = () => (
+  <i className="fa fa-heart like-button" />
+)
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+)
 
 
 
